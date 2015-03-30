7f90b887ac86fee0ab76f8145a08b70223a98051
TheMarkitecht <Mark@TheMarkitecht.com>
2015-03-30 01:30:49 +0000

    Notes on design direction.  No functional change.
    git-svn-id: svn://Laptop:port/personal/2014/camaro_ecm@506 58d5e4a1-9bfd-334e-a56e-dcfcd10d7f08

--- trunk/DE0_nano/source/bus_expander.v
+++ trunk/DE0_nano/source/bus_expander.v
@@ -8,6 +8,24 @@
     // could be the different assemble.tcl affected the timing...
     // but i'm back to 86 MHz after more very slight changes.
 // note: at least 8 bits of the data bus width were optimized away, so area is probably optimistic.
+
+/*
+//patch: this version needs some improvement.
+it's not actually removing the expanded space from the core space.  it's just
+providing another 16 address bits, and those bits are stored outside of the exr,
+in the preceding instruction.  
+the expanded space is still mux'd into the bus on the same cycle as any other data.
+so the reg-to-reg path is actually much longer with the expander inserted.
+
+right now the reduced core address space is making up for that with a small speed boost.
+but that balance will be severely upset when the expanded space scales up to hundreds or thousands.
+that's likely to happen when/if i add enough gp regs to make each event handler into its
+own state machine.  but that's really well outside the scope of the synapse316 concept.
+
+a good place to start would be declaring 1 cycle latency when reading through the expander, 
+much like i did for the core's adders.  then implement that latency as a reg
+in the expander's data path.  macro a similar solution for the writing through the expander.
+*/
         
 module bus_expander #(
      parameter NUM_REGS          = 65536       
