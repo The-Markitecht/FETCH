9f15e8c9091bdd430d53b95887843a1e13715425
TheMarkitecht <Mark@TheMarkitecht.com>
2015-04-05 21:59:26 +0000

    WIP synapse316_uart.v
    git-svn-id: svn://Laptop:port/personal/2014/camaro_ecm@521 58d5e4a1-9bfd-334e-a56e-dcfcd10d7f08

--- trunk/DE0_nano/source/synapse316_uart.v
+++ trunk/DE0_nano/source/synapse316_uart.v
@@ -17,9 +17,6 @@ module synapse316_uart #(
     
     , input wire[15:0]                data_in
     , input wire                      atx_reg_load
-    ,output wire                      atx_fifo_empty
-    ,output wire                      atx_fifo_full
-    ,output wire                      atx_busy
     
     ,output wire[15:0]                arx_reg_out        
     , input wire                      arx_reg_read
@@ -27,25 +24,18 @@ module synapse316_uart #(
     ,output wire                      arx_fifo_full
     ,output wire                      arx_busy
 );  
-
     // full duplex UART and its FIFO's and status register.  the FIFO's cross from sysclk domain to clk_async domain.
     // FIFO also provides a bit of RAM for storing up sequences until they can be transmitted.
     // FIFO's must be configured in "show-ahead" mode where rdreq is used as a read acknowledge.
     
     // TX section (atx) (transmitter).
-    wire[15:0] atxd;
-    std_reg #(.WIDTH(8)) atx_data_reg(sysclk, sysreset, atxd, r_load_data[7:0], r_load[`DR_ATX_DATA]);
-    wire[15:0] atxc;
-    wire txbsy;
-    wire rxbsy;
-    assign status_out = {atxc[15:3], rxbsy, txbsy, atxc[0]};
-    std_reg #(.WIDTH(1)) atx_ctrl_reg(sysclk, sysreset, atxc, r_load_data[0], r_load[`DR_ATX_CTRL]);
-    wire ;
-    wire[7:0] atx_fifo_data_out;
-    reg last_async_tx_busy = 1'd0;
-    always @(posedge clk921600)
-        last_async_tx_busy <= async_tx_busy;
-    wire atx_fifo_read_ack = async_tx_busy && ! last_async_tx_busy; // detect only the rising edge of async_tx_busy, to avoid draining atx_fifo prematurely.
+    assign status_out[15:6] = 0;
+    wire[7:0] atx_par;
+    wire atx_busy, atx_fifo_empty, atx_fifo_full;
+    reg last_atx_busy = 1'd0;
+    always @(posedge clk_async)
+        last_atx_busy <= atx_busy;
+    wire atx_fifo_read_ack = atx_busy && ! last_atx_busy; // detect only the rising edge of atx_busy, to avoid draining atx_fifo prematurely.
     synapse316_uart_fifo atx_fifo (
         .wrclk ( sysclk ),
         .data ( data_in[7:0] ),
@@ -53,25 +43,53 @@ module synapse316_uart #(
         .wrfull ( atx_fifo_full ),
 
         .rdclk ( clk_async ),
-        .q ( atx_fifo_data_out ),
+        .q ( atx_par ),
         .rdreq ( atx_fifo_read_ack ),
         .rdempty ( atx_fifo_empty )
     );    
     uart_v2_tx utx (
          .uart_sample_clk(clk_async) // clocked at 4x bit rate.
-        ,.parallel_in    (atxd[7:0])
-        ,.load_data      (r[`DR_ATX_CTRL][0])
+        ,.parallel_in    (atx_par)
+        ,.load_data      ( ! (atx_busy || atx_fifo_empty))
         ,.tx_line        (async_tx_line)
-        ,.tx_busy        (txbsy)
+        ,.tx_busy        (atx_busy)
     );    
+    assign r[`DR_ATX_DATA][15:8] = 8'd0;
     
     // RX section (arx) (receiver).
+    wire[7:0] arx_par;
+    wire arx_busy, arx_fifo_empty, arx_fifo_full;
+    reg last_arx_busy = 1'd0;
+    always @(posedge clk_async)
+        last_arx_busy <= arx_busy;
+    wire arx_fifo_write = last_arx_busy && ! arx_busy; // detect only the falling edge of arx_busy, to avoid filling arx_fifo prematurely.
+    synapse316_uart_fifo arx_fifo (
+        .wrclk ( clk_async ),
+        .data ( arx_par ),
+        .wrreq ( arx_fifo_write ),
+        .wrfull ( arx_fifo_full ),
+
+        .rdclk ( sysclk ),
+        .q ( arx_reg_out ),
+        .rdreq ( arx_reg_read ),
+        .rdempty ( arx_fifo_empty )
+    );    
     uart_v2_rx urx (
          .uart_sample_clk(clk_async) // clocked at 4x bit rate.
         ,.rx_line        (async_rx_line)
-        ,.rx_busy        (rxbsy)
-        ,.parallel_out   (r[`DR_ATX_DATA])
+        ,.rx_busy        (arx_busy)
+        ,.parallel_out   (arx_par)
     );
-    assign r[`DR_ATX_DATA][15:8] = 8'd0;
 
+    // status register.  read-only.
+    wire[3:0] status_comb = {atx_fifo_empty, atx_fifo_full, atx_busy, arx_fifo_full, arx_busy};
+    syncer3 #(width=4) status_syncer(clk_async, sysclk, status_comb, status_sync);
+    assign status_out[15:6] = 0;
+    assign status_out[`] = status_sync[`];
+    assign status_out[`] = status_sync[`];
+    assign status_out[`] = status_sync[`];
+    assign status_out[`] = status_sync[`];
+    assign status_out[`] = arx_fifo_empty; // no need to sync; this is already in sysclk domain.
+    assign status_out[`] = atx_fifo_full;  // no need to sync; this is already in sysclk domain.
+    
 endmodule
