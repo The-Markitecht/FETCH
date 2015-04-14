f977cfe8a9c299728a0df011865c331930b201bd
TheMarkitecht <Mark@TheMarkitecht.com>
2015-04-14 20:31:29 +0000

    WIP syntax for RAM access.
    git-svn-id: svn://Laptop:port/personal/2014/camaro_ecm@547 58d5e4a1-9bfd-334e-a56e-dcfcd10d7f08

--- trunk/DE0_nano/source/synapse/program/program_macros.tcl
+++ trunk/DE0_nano/source/synapse/program/program_macros.tcl
@@ -20,13 +20,16 @@ namespace eval ::asm {
     }
     
     proc ram_define {lin name {size_bytes 2}} {
-        # this must increment after memorizing, because the size may vary from one call to the next.
-        vdefine32 $lin $name $::asm::ram_counter
-        incr ::asm::ram_counter $size_bytes        
+        # ram_counter must increment after memorizing, because the size may vary from one call to the next.
+        set a [ram_join $::asm::ram_counter]
+        vdefine32 $lin $name $a
+        set ::asm::ram_counter [ram_split [expr {$a + $size_bytes}]]
     }
 
     proc ram {lin left eq right} {
-        if {[string first {:} $left] >= 0} {
+        # read or write RAM depending on direction of the assignment statement 
+        # (which side has the RAM address in hi'lo format).
+        if {[string first {.} $left] >= 0} {
             ram_write $lin $left $eq $right
         } else {
             ram_read $lin $left $eq $right
@@ -34,7 +37,7 @@ namespace eval ::asm {
     }
     
     proc ram_read {lin reg_name eq addr} {
-        lassign [split $addr {:}] hi lo
+        lassign [split $addr {.}] hi lo
         parse3 av_ad_hi $eq [expr {$hi}] "$lin // av_ad_hi = $hi"
         parse3 av_ad_lo $eq [expr {$lo}] "$lin // av_ad_lo = $lo"
         parse3 $reg_name $eq av_write_data "$lin // $reg_name = av_write_data // start read cycle"
@@ -42,10 +45,26 @@ namespace eval ::asm {
     }
 
     proc ram_write {lin addr eq reg_name} {
-        lassign [split $addr {:}] hi lo
+        lassign [split $addr {.}] hi lo
         parse3 av_ad_hi $eq [expr {$hi}] "$lin // av_ad_hi = $hi"
         parse3 av_ad_lo $eq [expr {$lo}] "$lin // av_ad_lo = $lo"
         parse3 av_write_data $eq $reg_name "$lin // av_write_data = $reg_name"
     }
 
+    proc ram_split {addr} {
+        # pass an address in Tcl integer format.
+        # returns the address in hi:lo format, for use with other macros.
+        
+        set hi [expr {$addr >> 16}]
+        set lo [expr {$addr & 0xffff}]
+        return ${hi}'$lo
+    }
+    
+    proc ram_join {addr} {
+        # pass an address in hi:lo format.  (Tcl integer is also acceptable.)
+        # returns the address in Tcl integer format.        
+        if {[string first {.} $addr] < 0} {return [expr {$addr}]}
+        lassign [split $addr {.}] hi lo
+        return [expr {($hi << 16) | $lo}]
+    }
 }    
