da65c3fb4aafb50a23028b90eaa45d801c6dbc22
TheMarkitecht <Mark@TheMarkitecht.com>
2015-09-10 03:35:29 +0000

    UNTESTED. Added a linear enrichment function to warmup plan.
    git-svn-id: svn://Laptop:port/personal/2014/camaro_ecm@600 58d5e4a1-9bfd-334e-a56e-dcfcd10d7f08

--- trunk/DE0_nano/source/synapse/program/plan_warmup.asm
+++ trunk/DE0_nano/source/synapse/program/plan_warmup.asm
@@ -1,7 +1,13 @@
 
 setvar warmup_success_temp_adc      0x4c0
     // 0x4c0 = 1216 = 120 degF at the sensor location outside the engine block.
-
+setvar warmup_min_temp_adc          750
+    // 750 = 0 degF.
+setvar warmup_max_enrichment_us     6000
+setvar warmup_us_per_cold_adc       (int($warmup_max_enrichment_us / ($warmup_success_temp_adc - $warmup_min_temp_adc)))
+setvar warmup_min_puff_len_us       4500
+    // max 6000 & min 4500 over a temp range 750 to 0x4c0 gives 6900 us puff at 74 deg F.  program rev 594.
+    
 :plan_name_warmup
     "WM\x0"
 
@@ -19,8 +25,41 @@ func destroy_plan_warmup
 end_func
 
 func puff_len_warmup
-    ram $ram_next_puff_len_us = 5000
-    //patch: add the dial setting to this, for testing.
+    // x = enrichment us.
+    x = 0
+    
+    // check engine block temp sensor.  if valid, enrich by a linear function:
+    // {warmup_min_temp_adc...warmup_success_temp_adc} -> {$warmup_max_enrichment_us...0}
+    a = $anmux_engine_block_temp
+    struct_read $ram_last_anmux_data
+    // b = sensor reading in adc counts.
+    a = $temp_ceiling_adc
+    br lt :temp_invalid
+        a = $warmup_success_temp_adc
+        br lt :too_warm
+            a = $warmup_min_temp_adc
+            br lt :in_range
+                b = $warmup_min_temp_adc
+            :in_range        
+            // negate the sensor reading so as to subtract it from warmup_success_temp_adc.
+            a = 0xffff
+            b = xor
+            a = 1
+            b = a+b
+            a = $warmup_success_temp_adc
+            b = a+b
+            // b = how many counts "cold" we are.  multiply that by the slope warmup_us_per_cold_adc.
+            a = $warmup_us_per_cold_adc
+            call :multiply         
+            x = a
+        :too_warm
+    :temp_invalid
+    
+    // apply total of base amount and enrichment.
+    // $warmup_min_puff_len_us <= total <= ($warmup_min_puff_len_us + $warmup_max_enrichment_us)
+    a = $warmup_min_puff_len_us
+    b = x
+    ram $ram_next_puff_len_us = a+b
 end_func
 
 func leave_warmup
@@ -33,10 +72,10 @@ func leave_warmup
     struct_read $ram_last_anmux_data
     a = $temp_ceiling_adc
     br lt :stay
-    a = $warmup_success_temp_adc
-    br gt :stay
-        call :destroy_plan_warmup
-        call :init_plan_run
+        a = $warmup_success_temp_adc
+        br gt :stay
+            call :destroy_plan_warmup
+            call :init_plan_run
     :stay
     
     :done
