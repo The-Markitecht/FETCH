06423c568088840a635a0188103cf95030bd522c
TheMarkitecht <Mark@TheMarkitecht.com>
2016-06-23 04:04:39 +0000

    WIP mapper tool. Implemented color space.
    git-svn-id: svn://Laptop:port/personal/2014/camaro_ecm@667 58d5e4a1-9bfd-334e-a56e-dcfcd10d7f08

--- trunk/PC_TCL/mapper/mapper.tcl
+++ trunk/PC_TCL/mapper/mapper.tcl
@@ -16,6 +16,11 @@ proc clear_sent_row {row} {
 }
 
 proc load_afrc_map {fn} {
+    set ::trim_half    0
+    set ::trim_unity   8192
+    set ::trim_double  24576
+    set ::trim_min     $::trim_half
+    set ::trim_max     $::trim_double
     set f [open $fn r]
     set map [read $f]
     close $f
@@ -46,16 +51,51 @@ proc send_afrc_map {} {
     }
 }
 
-proc show_cell {cnv id row col} {
-    $cnv coords cell_text [lrange [$cnv coords $id] 0 1]
-    set v [lindex $::afrc_map $row $col]
-    $cnv itemconfigure cell_text -state normal -text $v
-    puts [$cnv itemconfigure cell_text]
+proc background_error {msg} {
+    puts "background error:\n$msg\n$errorInfo"
+    exit
+}
+
+proc mapget {col row} {
+    return [lindex $::afrc_map $row $col]
+}
+
+proc mapset {col row value} {
+    if {$value < $::trim_min} {set value $::trim_min}
+    if {$value > $::trim_max} {set value $::trim_max}
+    lset ::afrc_map $row $col $value
+}
+
+proc bounds {col row} {
+    return [list [e $col * 10 + 10] [e $row * 10 + 10] [e $col * 10 + 19] [e $row * 10 + 19]] 
+}
+
+proc color {value} {
+    set i [e {int(($value - $::trim_min) * 255 / ($::trim_max - $::trim_min))}]
+    return [format #%02x%02x%02x $i $i 0]
+}
+
+proc refresh_cell {col row} {
+    puts "$col $row = [mapget $col $row]"
+    .win.c itemconfigure ${col},$row -fill [color [mapget $col $row]]
+}
+
+proc show_cell {cnv id col row} {
+    set ::cell_text "[lindex $::afrc_map $row $col] at row $row col $col"
+    lassign [$cnv coords $id] x y
+    $cnv coords cell_text $x [e {$y - 5}] 
+    $cnv itemconfigure cell_text -state normal 
+    #puts [$cnv itemconfigure cell_text]
+}
+
+proc hide_cell {cnv id col row} {
+    $cnv itemconfigure cell_text -state hidden 
 }
 
 proc init_gui {} {
 #    console show ;# useless
     fconfigure stdout -buffering line
+    interp bgerror {} background_error
     
     wm withdraw .
     set w .win
@@ -70,37 +110,44 @@ proc init_gui {} {
     button $b -text Send -command send_afrc_map
     pack $b 
     
-#    set status ${w}.status
-#    entry $status -textVariable ::status 
-#    pack $status -side top -expand yes -fill x  
-
     set c ${w}.c
-    canvas $c -relief flat -borderwidth 2 -width 500 -height 700
+    canvas $c -relief sunken -borderwidth 6 -width 500 -height 700
     pack $c -side top -expand yes -fill both
 
     for {set row 0} {$row < $::afrc_maf_rows} {incr row} {
         for {set col 0} {$col < $::afrc_rpm_cols} {incr col} {
-            set id [$c create rectangle \
-                [e $col * 10] [e $row * 10] [e $col * 10 + 9] [e $row * 10 + 9] \
-                -outline black -fill red -tags [list ${col},$row c$col r$row]]
+            set id [$c create rectangle [bounds $col $row] \
+                -tags [list ${col},$row c$col r$row]]
             $c bind $id <Enter> "
-                show_cell $c $id $row $col
+                show_cell $c $id $col $row
             "
+            $c bind $id <Leave> "
+                hide_cell $c $id $col $row
+            "
+            # test gradient: mapset $col $row [e $::trim_double * $col * $row / $::afrc_rpm_cols / $::afrc_maf_rows]
+            set dx [e $col - 12]
+            set dy [e $row - 41]
+            mapset $col $row [e $::trim_double - $dx * $dx * 100 - $dy * $dy * 100]
+            refresh_cell $col $row
         }
     }
-    
-    $c create text 0 0 -tags cell_text -state hidden -anchor s
+    set t ${w}.cell_text
+    set ::cell_text {}
+    entry $t -textvariable ::cell_text 
+    $c create window 0 0 -tags cell_text -state hidden -window $t -height 25 -width 140 -anchor s
 }
 
 # #########################################################
 
 if {[catch {
+    
     load_afrc_map default.map
-
+    
     init_gui
-
+    
     vwait forever
+    
 } err]} {
-    puts $err
-    vwait forever
+    puts "foreground error: \n$err\n$errorInfo"
+    exit
 }
